119359929276efadfcd07cc2efa4c9a34148ccd9
Wesarut Khm <anubis.smile@gmail.com>
2019-09-28 05:42:46 +0700

    Improvement - Write API for find addres by id.

--- src/main/java/com/pezy/pezy_api/controller/UserAddressController.java
+++ src/main/java/com/pezy/pezy_api/controller/UserAddressController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,25 +14,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pezy.pezy_api.entity.UserAddress;
+import com.pezy.pezy_api.pojo.ResponseMessage;
 import com.pezy.pezy_api.service.UserAddressService;
 
 @RestController
 @RequestMapping("/v1/user-address")
 @CrossOrigin(origins = "*")
 public class UserAddressController {
-	
+
 	@Autowired
 	private UserAddressService service;
-	
+
+	private ResponseMessage fMsg = new ResponseMessage();
+
 	@PostMapping
-	public ResponseEntity<?> create(@RequestBody UserAddress address){
+	public ResponseEntity<?> create(@RequestBody UserAddress address) {
 		address.setId(null);
 		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(address));
 	}
-	
+
 	@PutMapping
 	public ResponseEntity<?> update(@RequestBody UserAddress address) {
 		return ResponseEntity.ok(service.save(address));
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> findById(@PathVariable("id") Long id){
+		return ResponseEntity.ok(service.findById(id));
+	}
+
 }
--- src/main/java/com/pezy/pezy_api/service/UserAddressService.java
+++ src/main/java/com/pezy/pezy_api/service/UserAddressService.java
@@ -1,5 +1,7 @@
 package com.pezy.pezy_api.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,10 @@ public class UserAddressService {
 	
 	public UserAddress save(UserAddress address) {
 		return repository.save(address);
+	}
+	
+	public Optional<UserAddress> findById(Long id) {
+		return repository.findById(id);
 	}
 	
 }
