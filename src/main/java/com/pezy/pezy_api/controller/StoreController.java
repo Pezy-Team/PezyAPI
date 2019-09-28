a015e1971122ccc857f13efb1d5f202374cfe878
Wesarut Khm <anubis.smile@gmail.com>
2019-09-28 11:27:25 +0700

    Improvement - Upload image and file for document and picture store owner's authentication.

--- src/main/java/com/pezy/pezy_api/controller/StoreController.java
+++ src/main/java/com/pezy/pezy_api/controller/StoreController.java
@@ -1,18 +1,27 @@
 package com.pezy.pezy_api.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.pezy.pezy_api.entity.Store;
+import com.pezy.pezy_api.pojo.UploadFileResponse;
+import com.pezy.pezy_api.service.FileStorageService;
 import com.pezy.pezy_api.service.StoreService;
+import com.pezy.pezy_api.utils.FileUploadUtils;
 
 @RestController
 @RequestMapping("/v1/store")
@@ -21,6 +30,9 @@ public class StoreController {
 
 	@Autowired
 	private StoreService service;
+	
+	@Autowired
+	private FileStorageService fileService;
 	
 	@PostMapping
 	public ResponseEntity<?> create(@RequestBody Store store){
@@ -32,7 +44,54 @@ public class StoreController {
 	public ResponseEntity<?> update(@RequestBody Store store){
 		return ResponseEntity.ok(service.save(store));
 	}
-	
+
+	@PostMapping("/logo/upload")
+    public UploadFileResponse uploadLogo(@RequestParam MultipartFile file) {
+		FileUploadUtils fileUtils = new FileUploadUtils();
+		return fileUtils.uploadFile(file, fileService, "/v1/store/logo");
+	}
+
+    @GetMapping("/logo/{fileName:.+}")
+    public ResponseEntity<Resource> downloadLogo(@PathVariable String fileName, HttpServletRequest request) {
+    	FileUploadUtils fileUtils = new FileUploadUtils();
+    	return fileUtils.downloadFile(fileName, request, fileService);
+    }
+
+	@PostMapping("/id-card/upload")
+    public UploadFileResponse uploadIdCard(@RequestParam MultipartFile file) {
+		FileUploadUtils fileUtils = new FileUploadUtils();
+		return fileUtils.uploadFile(file, fileService, "/v1/store/id-card");
+	}
+
+    @GetMapping("/id-card/{fileName:.+}")
+    public ResponseEntity<Resource> downloadIdCard(@PathVariable String fileName, HttpServletRequest request) {
+    	FileUploadUtils fileUtils = new FileUploadUtils();
+    	return fileUtils.downloadFile(fileName, request, fileService);
+    }
+
+	@PostMapping("/id-card-with-face/upload")
+    public UploadFileResponse uploadIdCardWithFace(@RequestParam MultipartFile file) {
+		FileUploadUtils fileUtils = new FileUploadUtils();
+		return fileUtils.uploadFile(file, fileService, "/v1/store/id-card-with-face");
+	}
+
+    @GetMapping("/id-card-with-face/{fileName:.+}")
+    public ResponseEntity<Resource> downloadIdCardWithFace(@PathVariable String fileName, HttpServletRequest request) {
+    	FileUploadUtils fileUtils = new FileUploadUtils();
+    	return fileUtils.downloadFile(fileName, request, fileService);
+    }
+
+	@PostMapping("/bookbank/upload")
+    public UploadFileResponse uploadBookbank(@RequestParam MultipartFile file) {
+		FileUploadUtils fileUtils = new FileUploadUtils();
+		return fileUtils.uploadFile(file, fileService, "/v1/store/bookbank");
+	}
+
+    @GetMapping("/bookbank/{fileName:.+}")
+    public ResponseEntity<Resource> downloadBookbank(@PathVariable String fileName, HttpServletRequest request) {
+    	FileUploadUtils fileUtils = new FileUploadUtils();
+    	return fileUtils.downloadFile(fileName, request, fileService);
+    }
 	
 	@GetMapping("/structure")
 	public ResponseEntity<?> getStructure(){
